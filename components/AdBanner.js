9dd9ad9c1d16fcd8f9a9c1c7a44f2ba562376a72
Zubair Ahmed <zubair1024@gmail.com>
2020-11-08 22:42:17 +0530

    style changes

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -12,20 +12,21 @@ const AdBanner = () => {
 
   return (
     <Flex align="center" justify="center" flexDirection="column">
-      <Stack isInline>
-        {/* <Image src="https://via.placeholder.com/250"></Image> */}
-        <ins
-          className="adsbygoogle adbanner-customize"
-          style={{
-            display: 'block',
-            textAlign: 'center'
-          }}
-          data-ad-layout="in-article"
-          // data-ad-format="fluid"
-          data-ad-client="ca-pub-5766744601171066"
-          data-ad-slot="7040455455"
-        />
-      </Stack>
+      {/* <Stack isInline> */}
+      {/* <Image src="https://via.placeholder.com/250"></Image> */}
+      <ins
+        className="adsbygoogle adbanner-customize"
+        style={{
+          display: 'block'
+          //   textAlign: 'center',
+          //   minWidth: '250px'
+        }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-5766744601171066"
+        data-ad-slot="7040455455"
+      />
+      {/* </Stack> */}
     </Flex>
   );
 };
