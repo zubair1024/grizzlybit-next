69eac4b7e632fbd54356c2c07a04628262b3b1a7
Zubair Ahmed <zubair1024@gmail.com>
2020-11-08 21:36:32 +0530

    better styling for ad unit

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -12,22 +12,20 @@ const AdBanner = () => {
 
   return (
     <Flex align="center" justify="center" flexDirection="column">
-      {/* <Stack isInline> */}
-      {/* <Image src="https://via.placeholder.com/250"></Image> */}
-      <ins
-        className="adsbygoogle adbanner-customize"
-        style={{
-          display: 'block',
-          textAlign: 'center',
-          minWidth: '250px'
-        }}
-        data-adtest="on"
-        data-ad-layout="in-article"
-        data-ad-format="fluid"
-        data-ad-client="ca-pub-5766744601171066"
-        data-ad-slot="7040455455"
-      />
-      {/* </Stack> */}
+      <Stack isInline>
+        {/* <Image src="https://via.placeholder.com/250"></Image> */}
+        <ins
+          className="adsbygoogle adbanner-customize"
+          style={{
+            display: 'block',
+            textAlign: 'center'
+          }}
+          data-ad-layout="in-article"
+          // data-ad-format="fluid"
+          data-ad-client="ca-pub-5766744601171066"
+          data-ad-slot="7040455455"
+        />
+      </Stack>
     </Flex>
   );
 };
