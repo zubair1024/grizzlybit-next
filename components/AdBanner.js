31e1156ef085206bbd7963f978e852a38ef35242
Zubair Ahmed <zubair1024@gmail.com>
2020-11-08 21:33:47 +0530

    reshaping unit

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Flex, Image, Stack, Box } from '@chakra-ui/core';
 
 const AdBanner = () => {
   useEffect(() => {
@@ -10,20 +11,24 @@ const AdBanner = () => {
   }, []);
 
   return (
-    // <img src="https://via.placeholder.com/250"></img>
-    <ins
-      className="adsbygoogle adbanner-customize"
-      style={{
-        display: 'block',
-        textAlign: 'center',
-        minWidth: '250px'
-      }}
-      data-adtest="on"
-      data-ad-layout="in-article"
-      data-ad-format="fluid"
-      data-ad-client="ca-pub-5766744601171066"
-      data-ad-slot="7040455455"
-    />
+    <Flex align="center" justify="center" flexDirection="column">
+      {/* <Stack isInline> */}
+      {/* <Image src="https://via.placeholder.com/250"></Image> */}
+      <ins
+        className="adsbygoogle adbanner-customize"
+        style={{
+          display: 'block',
+          textAlign: 'center',
+          minWidth: '250px'
+        }}
+        data-adtest="on"
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-5766744601171066"
+        data-ad-slot="7040455455"
+      />
+      {/* </Stack> */}
+    </Flex>
   );
 };
 
