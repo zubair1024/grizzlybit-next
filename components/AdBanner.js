f3053e18df16e5a797b0bf120814a6edea600631
Zubair Ahmed <zubair1024@gmail.com>
2020-11-08 21:17:48 +0530

    minor changes to styling

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -10,12 +10,15 @@ const AdBanner = () => {
   }, []);
 
   return (
+    // <img src="https://via.placeholder.com/250"></img>
     <ins
       className="adsbygoogle adbanner-customize"
       style={{
         display: 'block',
-        textAlign: 'center'
+        textAlign: 'center',
+        width: '250px'
       }}
+      data-adtest="on"
       data-ad-layout="in-article"
       data-ad-format="fluid"
       data-ad-client="ca-pub-5766744601171066"
